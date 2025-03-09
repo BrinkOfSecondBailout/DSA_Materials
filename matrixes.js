2f1a45fde6fe54da6bbe7b960917e4a2da9cfafc
Danny Nguyen <nguyenduongpa@gmail.com>
2025-03-08 19:02:30 -0800

    searchMatrix

--- matrixes.js
+++ matrixes.js
@@ -178,3 +178,36 @@ var setZeroes = function(matrix) {
 
 
 
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    if (m === 0 || n === 0) return false;
+
+    let left = 0;
+    let right = m * n - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2); // index
+        const row = Math.floor(mid / n); // index / col = row location
+        const col = mid % n; // index modular col = col location
+
+        let value = matrix[row][col];
+        if (value === target) {
+            return true;
+        } else if (value < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+};
+// let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
+// let target = 3;
+
+// console.log(searchMatrix(matrix, target));
+
+
+
+

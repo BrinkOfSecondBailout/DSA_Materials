35ddc4d4597a0f94e608b840575971755c6f40cb
Danny <nguyenduongpa@gmail.com>
2025-03-10 20:42:29 -0700

    romans

--- strings.js
+++ strings.js
@@ -154,3 +154,36 @@ var convert = function(s, numRows) {
 
 // https://leetcode.com/problems/zigzag-conversion/
 
+
+var intToRoman = function(num) {
+    const romans = [
+        { value: 1000, symbol: 'M' },
+        { value: 900, symbol: 'CM' },
+        { value: 500, symbol: 'D' },
+        { value: 400, symbol: 'CD' },
+        { value: 100, symbol: 'C' },
+        { value: 90, symbol: 'XC' },
+        { value: 50, symbol: 'L' },
+        { value: 40, symbol: 'XL' },
+        { value: 10, symbol: 'X' },
+        { value: 9, symbol: 'IX' },
+        { value: 5, symbol: 'V' },
+        { value: 4, symbol: 'IV' },
+        { value: 1, symbol: 'I' }
+    ]
+    let result = "";
+    while (num > 0) {
+        for (const item of romans) {
+            if (num >= item.value) {
+                result += item.symbol;
+                num -= item.value;
+                break;
+            }
+        }
+    }
+    return result;
+};
+
+// const num = 1994;
+// console.log(intToRoman(num));
+// https://leetcode.com/problems/integer-to-roman/description/

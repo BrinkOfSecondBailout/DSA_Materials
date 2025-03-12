d1f55e2b665d963436d10100c4b86de908aeeb1b
Danny Nguyen <nguyenduongpa@gmail.com>
2025-03-12 08:15:25 -0700

    binary search

--- array.js
+++ array.js
@@ -19,3 +19,93 @@ var twoSum = function(nums, target) {
 // https://leetcode.com/problems/two-sum/description/
 
 
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let result = [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return result;
+};
+
+// const nums = [0,0,0,0];
+// console.log(threeSum(nums));
+// https://leetcode.com/problems/3sum/description/
+
+
+
+
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        let width = right - left;
+        let currentArea = Math.min(height[left], height[right]) * width;
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+}
+
+
+// const height = [1,1];
+// console.log(maxArea(height));
+// https://leetcode.com/problems/container-with-most-water/
+
+
+
+
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        if (nums[left] <= nums[mid]) { // Left part is sorted
+            if (nums[left] <= target && target < nums[mid]) { // Target must be in the left half
+                right = mid - 1;
+            } else { // Target in the right half
+                left = mid + 1;
+            }
+        } else { // Right part is sorted
+            if (nums[right] >= target && target > nums[mid]) { // Target must be in right half
+                left = mid + 1;
+            } else { // Target in the left half
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+// const nums = [4,5,6,7,0,1,2], target = 0;
+// console.log(search(nums, target));
+// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1339701128/
